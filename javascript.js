b568df49b398df4acac6437b1f4a6e9db0538230
cvanbeck <98055697+cvanbeck@users.noreply.github.com>
2022-03-03 04:06:50 +0000

    add vertical squares

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
-const box = document.querySelector(".box")
+/* So currently this will create a element with a class called box, it then fills each box with a
+inputted amount of divs called tests, to fix we need to create multiple boxs */
+
+
 function createDrawingGrid(size) {
     for (let i = 0; i < size;) {
-        const grid = document.createElement("div");
-        grid.classList.add("test")
-        box.append(grid);
+        const body = document.querySelector(".grid")
+        const box = document.createElement("box")
+        for (let x = 0; x < size;) {
+            const grid = document.createElement("div");
+            body.appendChild(box)
+            grid.classList.add("test")
+            box.appendChild(grid);
+            ++x
+        }
         ++i;
         console.log(i);
+
     }
 }
 
-createDrawingGrid(10)
+createDrawingGrid(18)
+
