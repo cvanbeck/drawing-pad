6228c13272a69cefd5fb4f7a59cb6a1ad631d370
cvanbeck <charlievanbeck@gmail.com>
2022-03-06 20:57:32 +0000

    Remove old grid generation function

--- javascript.js
+++ javascript.js
@@ -1,33 +1,14 @@
-/* So currently this will create a element with a class called box, it then fills each box with a
-inputted amount of divs called tests, to fix we need to create multiple boxs */
-
-
-function createDrawingGrid(size) {
-    for (let i = 0; i < size;) {
-        const body = document.querySelector(".grid")
-        const box = document.createElement("box")
-        for (let x = 0; x < size;) {
-            const grid = document.createElement("div");
-            body.appendChild(box)
-            grid.classList.add("test")
-            box.appendChild(grid);
-            ++x
-        }
-        ++i;
-        console.log(i);
-
-    }
-}
-
 function createGrid(size) {
     for (let i = 0; i < (size * size);) {
         const grid = document.querySelector(".grid");
         const box = document.createElement("box");
-        box.classList.add("test");
+        box.classList.add("actual-box");
         grid.appendChild(box);
         ++i
     }
     document.getElementsByClassName("grid")[0].style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
 
-createGrid(64)
+createGrid(10)
+
+
