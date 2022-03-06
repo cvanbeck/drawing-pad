198c64fe84b9f117e28ae4300dd04c1c12eac052
cvanbeck <charlievanbeck@gmail.com>
2022-03-06 21:44:22 +0000

    Add ability to colour a box black

--- javascript.js
+++ javascript.js
@@ -1,14 +1,23 @@
+let gridSize = 24
 function createGrid(size) {
     for (let i = 0; i < (size * size);) {
         const grid = document.querySelector(".grid");
         const box = document.createElement("box");
-        box.classList.add("actual-box");
         grid.appendChild(box);
         ++i
     }
     document.getElementsByClassName("grid")[0].style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
 
-createGrid(10)
+createGrid(gridSize)
 
+/* To draw a box, we need it to tell us which number the box has to find it's numbers and then we can
+add another class to this box so that only it gets coloured */ 
 
+let allBox = document.querySelectorAll("box")
+allBox.forEach((allBox) => {
+    allBox.addEventListener("click", () => {
+        allBox.classList.add("black-box")
+
+    });
+});
